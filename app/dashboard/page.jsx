11b7c01c63f221fc67a61841226e122632f465b7
Sharif-Miah <farhansharif141077@gmail.com>
2024-01-27 22:09:10 +0600

    nextauth setup and login and signup page done

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold mt-6'>This is Dashboard Page</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,11 @@
+import { SignIn } from '@clerk/nextjs';
+
+const SigninPage = () => {
+    return (
+        <div>
+           <SignIn />
+        </div>
+    );
+};
+
+export default SigninPage;
